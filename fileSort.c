ff50bff7632e56aec47f213193f20eb266a58b10
Graham <gdeubner@gmail.com>
2020-02-13 15:24:16 -0500

    Adding header file

--- fileSort.c
+++ fileSort.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <errno.h>
 #include <fcntl.h>
-
+#include "fileSort.h"
 
 
 int* numArray;
---
+++ fileSort.h
@@ -0,0 +1,15 @@
+#ifndef FILESORT_H
+#define FILESORT_H
+
+
+//node for storing each string token from the file
+typedef struct NODE{
+  char* val;
+  struct node* next;
+  
+}node;
+
+
+
+
+#endif
