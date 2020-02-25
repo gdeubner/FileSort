ff47b36b49c93bf3205a24e96853a0375d2d7966
Daniel Doan <dd891@ilab2.cs.rutgers.edu>
2020-02-25 02:24:51 -0500

    Added sorting methods, edited some compare methods to work with sorting, added basic error checking

--- fileSort.c
+++ fileSort.c
@@ -22,7 +22,125 @@ char* cleanStr(char*);
 void printLL(node*);
 node* parseString(char*, char, node*, char*);
 node* readFile(int, char*);
+int insertionSort(void*,int (*comparator)(void*,void*));
+int quickSort(void*,int (*comparator)(void*,void*));
+void split(void*,int,int,int (*comparator)(void*,void*));
 
+//recursive function that quicksorts given array
+void split(void* arr,int lo,int hi,int(*comparator)(void*,void*)){
+  if(fileType==1){
+    char** array=(char**)arr;
+    int i;
+    int j;
+    char* temp;
+    int pivot;
+    if(lo<hi){
+    i=lo;
+    j=hi;
+    pivot=lo; //first element is pivot
+    while(i<j){
+      while(comparator(array[i],array[pivot])==1&&i<hi){ //if array[i] comes before array[j] alphabetically
+	i++;
+      }
+      while(comparator(array[j],array[pivot])<1&&j>lo){ //if array[i] comes after array[j] alphabetically
+	j--;
+      }
+      if(i<j){ //swaps hi and lo index elements
+	  temp=array[i];
+	  array[i]=array[j];
+          array[j]=temp;
+        }
+      }
+    temp=array[pivot]; //putting pivot in right place
+      array[pivot]=array[j];
+      array[j]=temp;
+      split(array,lo,j-1,comparator);
+      split(array,j+1,hi,comparator);
+      return;
+     }
+  } else{
+    int** array=(int**)arr;
+    int i;
+    int j;
+    int* temp;
+    int pivot;
+    if(lo<hi){
+    i=lo;
+    j=hi;
+    pivot=lo;
+    while(i<j){
+      while(comparator(array[i],array[pivot])<1&&i<hi){ //if array[i] comes before array[j]
+	i++;
+      }
+      while(comparator(array[j],array[pivot])==1&&j>lo){ //if array[i] comes after array[j]
+	j--;
+      }
+      if(i<j){ //swaps hi and lo index elements
+	  temp=array[i];
+	  array[i]=array[j];
+          array[j]=temp;
+        }
+      }
+      temp=array[pivot]; //putting pivot in right place
+      array[pivot]=array[j];
+      array[j]=temp;
+      split(array,lo,j-1,comparator);
+      split(array,j+1,hi,comparator);
+      return;
+     }
+  }
+ }
+//"Shell" for quick sorting
+int quickSort(void* toSort,int (*comparator)(void*,void*)){
+  if(fileType==1){ //if filetype is strings
+  split(toSort,0,arraySize-1,comparator);
+  printArray(toSort,fileType);
+  } else { //if filetype is ints
+    char**array=(char**)toSort;
+    int** numArray=malloc(sizeof(int*)*arraySize);
+    int i;
+    for(i=0;i<arraySize;i++){
+      numArray[i]=atoi(array[i]);
+    }
+    split(numArray,0,arraySize-1,comparator);
+    printArray(numArray,fileType);
+  }
+  return 1;
+}
+int insertionSort(void* toSort,int (*comparator)(void*,void*)){
+  int i,j;
+  if(fileType==0){//if filetype is ints
+    char** sarray=(char**)toSort;
+    int** array=malloc(sizeof(int*)*arraySize);
+    for(i=0;i<arraySize;i++){
+      array[i]=atoi(sarray[i]); //creates proper integer array
+    }
+    for(i=1;i<arraySize;i++){
+      int* cmp=array[i];
+	j=i-1;
+	while(j>=0&&comparator(array[j],cmp)==1){//while element at index j is greater than element we are inserting
+	  array[j+1]=array[j];//moves element at index j ahead
+	  j--;
+	}
+	array[j+1]=cmp;
+    }
+    printArray(array,fileType);
+  } else{
+    char** array=malloc(sizeof(char*)*arraySize);
+    array=(char**)toSort;
+    for(i=1;i<arraySize;i++){
+      char* cmp=array[i];
+      j=i-1;
+      while(j>=0&&comparator(array[j],cmp)<1){ //while element at index j is alphabetically ahead element we are inserting
+	array[j+1]=array[j]; //moves element at index j ahead one
+	j--;
+      }
+      array[j+1]=cmp;
+    }
+    printArray(array,fileType);
+  }
+  return 1;
+}
 //takes array of char* or int, ft=fileType, 1 for char* and 0 for int
 void printArray(void* array, int ft){
   if(ft==1){// strings
@@ -50,7 +168,7 @@ int setFileType(node* head){
     if(strlen(ptr->str)==0)
       ptr = ptr->next;
     else{
-      if(isdigit(ptr->str[0]>0))
+      if(isdigit(ptr->str[0])>0||ptr->str[0]=='-')
 	 fileType = 0;
       else
 	fileType = 1;
@@ -70,7 +188,6 @@ void* makeArray(node* head){
   }
   ptr = head;
   int count = 0;
-  if(fileType==1){//letters
     char** array = malloc(sizeof(char*)*arraySize);
     while(ptr!=NULL){
       array[count] = ptr->str;
@@ -80,18 +197,6 @@ void* makeArray(node* head){
       count++;
     }
     return array;
-  }
-  else{//numbers
-    int* array = malloc(sizeof(int)*arraySize);
-    while(ptr!=NULL){
-      array[count] = atoi(ptr->str);
-      prev = ptr;
-      ptr = ptr->next;
-      free(prev);
-      count++;
-    }
-    return array;
-  }
 }
 
 //return 1 if A>B, -1 if B>A, and 0 if A=B
@@ -115,8 +220,12 @@ int compareChar(void* TOKENA, void* TOKENB){
   return 0;
 }
 int compareInt(void* TOKENA, void* TOKENB){
-  int* a = (int*)TOKENA;
-  int* b = (int*)TOKENB;
+  int a = (int)TOKENA;
+  int b = (int)TOKENB;
+  if(a<0&&b>=0)
+    return -1;
+  if(b<0&&a>=0)
+    return 1;
   if(a>b)
     return 1;
   if(b>a)
@@ -237,12 +346,32 @@ node* readFile(int arguments, char* fileName){
   return head;
 }
 int main (int argc, char** argv){
+  if(argc!=3||argv[2][0]!='-'){
+    printf("ERROR: Please enter two properly formatted commands\n");
+    return 0;
+  }
+  char command=argv[2][1];
+  if(command!='q'&&command!='i'){
+    printf("ERROR: Please enter two properly formatted commands\n");
+    return 0;
+  }
   node* head = readFile(argc, argv[1]);
-  //  printLL(head);
+  //printLL(head);
   setFileType(head);
-  char** array = (char**) makeArray(head);
-  //printf("%s\n", array[0]);
-  printArray(array, fileType);
-
+  char** array=(char**)makeArray(head);
+  if(fileType==1){
+    if(command=='q'){
+    quickSort(array,compareChar);
+    }else{
+    insertionSort(array,compareChar);
+    
+    }
+  }
+  if(fileType==0)
+    if(command=='q'){
+    quickSort(array,compareInt);
+    }else{
+    insertionSort(array,compareInt);
+    }
   return 0;
 }
